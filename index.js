d8823262a70c4c248ac85f00b1c05959c95ede80
Chris Jones <christopher.jones@callminer.com>
2021-03-05 12:03:22 -0500

    Search bot commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config()
+
+client.on('ready', () => {
+ console.log(`Logged in as ${client.user.tag}!`);
+ });
+
+client.on('message', msg => {
+ if (msg.content.startsWith('[[') && msg.content.endsWith(']]')) {
+     var l = msg.content.length;
+     const article = msg.content.slice(2, l-2);
+     const rad_reply = "Hi, I'm Rad, and I want to tell you about https://tfwiki.net/wiki/" + article;
+     msg.channel.send(rad_reply);
+ }
+});
+
+client.login(process.env.TFWIKISEARCH_BOT_TOKEN)
