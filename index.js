135f25c8f549599e3ec3dd0d5f586a7ecfe0572f
Chris Jones <christopher.jones@callminer.com>
2021-03-05 12:37:36 -0500

    Fix ALL the spaces

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on('message', msg => {
  if (msg.content.startsWith('[[') && msg.content.endsWith(']]')) {
      var l = msg.content.length;
      var article = msg.content.slice(2, l-2);
-     article = article.replace(" ", "_");
+     article = article.split(" ").join("_");
      const rad_reply = "Hi, I'm Rad, and I want to tell you about https://tfwiki.net/wiki/" + article;
      msg.channel.send(rad_reply);
  }
