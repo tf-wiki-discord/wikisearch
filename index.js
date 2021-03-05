88a8759b88eb51de2fbcdc326920dcb8b94cc32d
Chris Jones <christopher.jones@callminer.com>
2021-03-05 16:22:52 -0500

    Give Rad some friends

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.user.setUsername('GO!-Bot')
+client.user.setActivity('Hanging with Carlos and Alexis')
 require('dotenv').config()
 
 client.on('ready', () => {
