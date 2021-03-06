0eeca8d01d337fa605010e19cb6816778aa20d26
Chris Jones <christopher.jones@callminer.com>
2021-03-06 15:16:53 -0500

    Semicolon parse fix

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ client.on('message', msg => {
             const radEmbed = new Discord.MessageEmbed()
               .setColor('#0099ff')
               .setDescription(editpage.slice(boldStart, boldEnd+1)) 
-              .setTitle("Hi, my name's Rad, and I wanna tell you about " + pageName;)
-              .setURL(pageURL;)
+              .setTitle("Hi, my name's Rad, and I wanna tell you about " + pageName)
+              .setURL(pageURL)
 
             if(imageName) {
                 // get the direct image file path via Special:FilePath
