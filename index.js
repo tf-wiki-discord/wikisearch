f6f87508ce77e90878ba0fd1f83ded32da091874
Chris Jones <christopher.jones@callminer.com>
2021-03-06 14:27:00 -0500

    Rad reaction to wicked sweet

--- index.js
+++ index.js
@@ -77,6 +77,8 @@ client.on('message', msg => {
       });
     }
     else if (/wicked sweet/i.test(msg.content)) {
+       const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'aboutTheTransformers');
+       message.react(reactionEmoji);
        const sweetEmbed = new Discord.MessageEmbed().setImage('https://tfwiki.net/wiki/Special:FilePath/PaniniRadWhite.jpg');
        msg.channel.send(sweetEmbed)
     }
