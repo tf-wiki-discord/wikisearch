07a68b5aff14a9e771f9627d6e1628821bf98f56
Chris Jones <christopher.jones@callminer.com>
2021-03-06 01:40:19 -0500

    Axios to scrape TFWiki for Search Rad

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const axios = require('axios')
 require('dotenv').config()
 
 client.on('ready', () => {
@@ -13,8 +14,41 @@ client.on('message', msg => {
      var l = msg.content.length;
      var article = msg.content.slice(2, l-2);
      article = article.split(" ").join("_");
-     const rad_reply = "Hi, I'm Rad, and I want to tell you about https://tfwiki.net/wiki/" + article;
+     const fullWikiArticle = "https://tfwiki.net/wiki/" + article;
+     const rad_reply = "Hi, I'm Rad, and I want to tell you about " + fullWikiArticle;
      msg.channel.send(rad_reply);
+
+     const editURL = "https://tfwiki.net/mediawiki/index.php?title=" + article + "&action=edit"
+    axios.get(editURL, {
+      headers:{
+        Accept: 'accept',
+        Authorization: 'authorize'
+      },
+    }).then(response => {
+        if(response.status === 200) {
+            let editpage = response.data;
+            let re = /'''/;
+            let boldStart = editpage.search(re);
+            let boldEnd = editpage.indexOf(".", boldStart);
+            console.log(editpage.slice(boldStart, boldEnd+1));
+
+            let imageRE = /(Image:|File:).*jpg/;
+            const matches = editpage.match(imageRE)
+            var imageName;
+            if(matches) {
+                console.log(matches[0])
+                imageName = matches[0];
+            }
+            const radEmbed = Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setThumbnail(imageName)
+                .setDescription(editpage.slice(boldStart, boldEnd+1))
+
+        }
+      return response;
+    }).catch(err => {
+      console.log(err);
+    });
  }
 });
 
