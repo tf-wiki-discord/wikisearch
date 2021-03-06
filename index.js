cbb8b037d6953159479af3785389b382847b7914
Nicolas Bossons <34184475+TheWadapan@users.noreply.github.com>
2021-03-06 19:24:05 +0000

    fixed this, I think, and turned it into an embed

--- index.js
+++ index.js
@@ -76,10 +76,9 @@ client.on('message', msg => {
       console.log(err);
       });
     }
-    else if (msg.content.search(/wicked sweet/i)) {
-       const sweetRide = "https://tfwiki.net/wiki/Special:FilePath/PaniniRadWhite.jpg"
-       msg.react(':aboutTheTransformers:') 
-       msg.channel.send("Wicked sweet! " + sweetRide)
+    else if (/wicked sweet/i.test(msg.content)) {
+       const sweetEmbed = new Discord.MessageEmbed().setImage('https://tfwiki.net/wiki/Special:FilePath/PaniniRadWhite.jpg');
+       msg.channel.send(sweetEmbed)
     }
   }
 });
