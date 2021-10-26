75a24ffce88f9a7410daa5d6cd673039eb31664a
tf-wiki-discord <80117467+tf-wiki-discord@users.noreply.github.com>
2021-10-25 21:10:13 -0400

    Remove guild info

--- index.js
+++ index.js
@@ -76,8 +76,6 @@ client.on('message', msg => {
         msg.channel.send("Bah!");
     }
     if( /^!!count$/.test(msg.content)) {
-	const guild = client.guilds.cache;
-	//console.log(guild);
 	const memberCount = client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);
 	console.log("Member count: ", memberCount);
 	msg.channel.send("Member count: ", memberCount.toString());
