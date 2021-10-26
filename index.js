c4c3ba6c9063c439d47c3116049988b993f2c832
tf-wiki-discord <80117467+tf-wiki-discord@users.noreply.github.com>
2021-10-26 02:03:33 -0400

    brace not paren

--- index.js
+++ index.js
@@ -82,12 +82,10 @@ client.on('message', msg => {
 				if(mc - d > 10) {
 					channel.send(`Old and new count: ${mc} vs ${d}`);
 				}
-		
-		
 				fs.writeFile("count.txt", mc.toString(), (err) => {
 					if(err) {throw err;}
 				});
-			}}
+			})
 			
 		})
 	}, 1.5 * 1000)
