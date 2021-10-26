d0dafa4fcffa83d91faadb68498aa956ca009132
tf-wiki-discord <80117467+tf-wiki-discord@users.noreply.github.com>
2021-10-25 23:30:27 -0400

    fetch channel ID

--- index.js
+++ index.js
@@ -74,7 +74,9 @@ client.on('message', msg => {
 	var interval = setInterval (function () {
 		const mc = client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);
 		console.log("Member count: ", mc);
-		client.channels.cache.get('817814828222775346').send(`Member count: ${mc}`).catch(console.error);
+		client.channels.fetch('817814828222775346')
+ 		 .then(channel => channel.send(`Member count: ${mc}`))
+		//client.channels.cache.get('817814828222775346').send(`Member count: ${mc}`).catch(console.error);
 	}, 1000)
     }
 	  
