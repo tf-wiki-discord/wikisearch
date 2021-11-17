a4827e24b477ce17c2f0af11479922ae97225711
Prime Radiant <voidimmutable@gmail.com>
2021-11-17 16:57:31 -0500

    Fix Rad regex

--- index.js
+++ index.js
@@ -77,9 +77,9 @@ client.on('message', msg => {
         msg.channel.send("Bah!");
     }
     
-   if (/[thanks|thank you](.*)rad/i.test(msg.content)) {
-       msg.react("<:aboutTheTransformers:656259059854344202>");
-   }
+//   if (/[thanks|thank you](.*)rad/i.test(msg.content)) {
+//       msg.react("<:aboutTheTransformers:656259059854344202>");
+//   }
       
     if (/!!gobox/.test(msg.content)) {
         const options = {
