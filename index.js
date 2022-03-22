b64c31631bfc64d67d7f015a4e574f06df63fb89
tf-wiki-discord <80117467+tf-wiki-discord@users.noreply.github.com>
2022-03-22 11:36:08 -0400

    Can't use await in non-async function

--- index.js
+++ index.js
@@ -90,12 +90,10 @@ client.on('message', msg => {
    }
 
    if (/!!lurkers/i.test(msg.content) && (msg.channel.name.includes("wreck-room") || msg.channel.name.includes("coding") || msg.channel.name.includes("moderation"))) {
-	  const guild = client.guilds.fetch('656241088826441729')
-	  const members = await guild.members.fetch()
-	  members.forEach(member => {
+	  const list = client.guilds.cache.get('656241088826441729')
+	  list.members.cache.forEach(member => {
 	  	console.log(member.user.username)
-		msg.channel.send(member.user.username)
-			  
+		msg.channel.send(member.user.username)	  
 	  })
    }
 	  
