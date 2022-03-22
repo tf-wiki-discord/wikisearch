0387b9b0223e43a8cbadcb2fda76137a63a98361
tf-wiki-discord <80117467+tf-wiki-discord@users.noreply.github.com>
2022-03-22 00:38:24 -0400

    Wrong Rad branch

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ client.on('message', msg => {
        msg.react("<:aboutTheTransformers:656259059854344202>");
    }
 
-   if (/!!rad (.*)/i.test(msg.content) && (!msg.author.bot && msg.member.roles.cache.has('656250893678936077'))) {
+   if (/!!lurkers/i.test(msg.content) && (!msg.author.bot && msg.member.roles.cache.has('656250893678936077'))) {
 	  msg.guild.members.fetch().then(members => {
 		  members.forEach(member => {
 			  console.log(member.user.username)
@@ -98,7 +98,7 @@ client.on('message', msg => {
 	  })
    }
 	  
-   if (/!!lurkers (.*)/i.test(msg.content) && (msg.channel.name.includes("wreck-room") || msg.channel.name.includes("coding") || msg.channel.name.includes("moderation"))) {
+   if (/!!rad (.*)/i.test(msg.content) && (msg.channel.name.includes("wreck-room") || msg.channel.name.includes("coding") || msg.channel.name.includes("moderation"))) {
  
        var eliza = new chatbot.ElizaBot();
        var chatbotinput = msg.content.match(/!!rad (.*)/)[1]
