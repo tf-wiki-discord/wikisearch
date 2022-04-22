469a462a21083e7436fa3af024fc292b12039470
tf-wiki-discord <80117467+tf-wiki-discord@users.noreply.github.com>
2022-04-21 20:07:46 -0400

    Handle special pages take 2

--- index.js
+++ index.js
@@ -365,16 +365,8 @@ client.on('message', msg => {
             }
         }
       }
-      else if(pageName == "Special:RecentChanges") {
-	embedTitle = `Hi ${author}, my name's Rad, and here's a link to recent changes on the wiki!`
-      }
-      else if(pageName == "Special:Random") {
-	embedTitle = `Hi ${author}, my name's Rad, and here's a page on the wiki!`
-      }
-      else {
-        embedTitle = `Hi ${author}, my name's Rad, and I'd like to tell you about ${pageName}, but I can't!`
         console.log(author + " sent " + pageName)
-      }
+
         if(/among us/i.test(pageName)
             || /amongus/i.test(pageName)
             || /amogus/i.test(pageName)
@@ -384,6 +376,15 @@ client.on('message', msg => {
             msg.channel.send(`Hi ${author}, my name's Rad, and I'd like to tell you about the Transformers instead of Among Us!`)
         }
         else {
+	 if(pageName == "Special:RecentChanges") {
+		embedTitle = `Hi ${author}, my name's Rad, and here's a link to recent changes on the wiki!`
+      	 }
+      	 else if(pageName == "Special:Random") {
+		embedTitle = `Hi ${author}, my name's Rad, and here's a page on the wiki!`
+      	 }
+	 else {
+		embedTitle = `Hi ${author}, my name's Rad, and I'd like to tell you about ${pageName}, but I can't!`
+	 }
             radEmbed.title = embedTitle
             msg.channel.send(radEmbed);
         }
