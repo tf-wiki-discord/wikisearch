84f2782679e503d917f7b593c510afda9332321d
tf-wiki-discord <80117467+tf-wiki-discord@users.noreply.github.com>
2022-07-14 01:24:35 -0400

    add promises

--- index.js
+++ index.js
@@ -101,8 +101,9 @@ client.on('message', msg => {
 	  //console.log(msg)
 	  //console.log(msg.embeds[0])
 	  const ytslug = msg.content.match(/youtube\.com\/watch\?v=(.*)/i)[1]
-	  const ytdata = getYTtitle(ytslug)
-	  console.log("YT title: ", ytdata)
+	  getYTtitle(ytslug).then(ytdata => {
+	  	console.log("YT title: ", ytdata)
+	  })
 	  
 	  const replaceEmbed = new MessageEmbed()
 		.setTitle("No Jobby-posting! Instead let\'s talk about anything else.")
