71ec3a4bc8be5806e0cface018598b54460d007d
Prime Radiant <voidimmutable@gmail.com>
2022-12-26 10:20:45 -0500

    misspelled description

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js')
 module.exports = {
 	data: new SlashCommandBuilder()
 	          .setName('ping')
-	          .setDesciption('Replies with pong!'),
+	          .setDescription('Replies with pong!'),
 	      async execute(interaction) {
 		      await interaction.reply('Pong!')
 	      },
