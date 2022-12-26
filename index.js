2d8daac88819969d039c6d87c42c7a8f3c810603
Prime Radiant <voidimmutable@gmail.com>
2022-12-26 10:25:32 -0500

    double typo in commands

--- index.js
+++ index.js
@@ -469,7 +469,7 @@ const commandFiles = fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'))
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
-	if('data' in commands && 'execute' in commands) {
+	if('data' in command && 'execute' in command) {
 		client.commands.set(command.data.name, command);
 	} else {
 		console.log(`[WARNING] The command at ${filePath} is missing required "data" or "execute" properties.`);
