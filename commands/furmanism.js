4881129c27c8dfb4579e3d9e2e8072b78e7b9169
tf-wiki-discord <80117467+tf-wiki-discord@users.noreply.github.com>
2023-01-15 19:04:43 -0500

    Add index ability to furmanism.js

--- commands/furmanism.js
+++ commands/furmanism.js
@@ -3,13 +3,16 @@ const { SlashCommandBuilder } = require('discord.js')
 function randomChoice(arr) {
 	return arr[Math.floor(arr.length * Math.random())];
 }
-const furmanisms = Array(
+function isNumeric(value) {
+    return /^\d+$/.test(value);
+}
+const furmanisms = Array("IT NEVER ENDS!","IT IS OVER -- FINISHED!",
 	"A SHORT, SHARP LESSON!", "A WHOLE WORLD OF PAIN!",
 	"ALL THE DIRTY JOBS...","CAN I DO LESS?",
 	"CANNOT, WILL NOT...!","DIE SCREAMING!",
 	"DOWN TO EARTH--LITERALLY!","FIGHT AND DIE!",
-	"IT CAN BE HURT!","IT IS OVER -- FINISHED!",
-	"IT NEVER ENDS!","LIKE SOME PREDATORY BIRD!",
+	"IT CAN BE HURT!",
+	"LIKE SOME PREDATORY BIRD!",
 	"NEVER DID WANT TO LIVE FOREVER!","NO! YOU'RE DEAD!",
 	"POWER BEYOND MEASURE!","REAP THE WHIRLWIND!",
 	"SURPLUS TO REQUIREMENTS!","THE WORST CASE OF INDIGESTION IT'S EVER HAD!",
@@ -17,12 +20,26 @@ const furmanisms = Array(
 	"WELL AND TRULY!","WHAT ARE YOU PLAYING AT?",
 	"WHAT CHANCE DO WE HAVE?","YOU WON'T BELIEVE THE THINGS I CAN DO NOW!"
 )
+const cmdlen = furmanisms.length
 
 module.exports = {
 	data: new SlashCommandBuilder()
 	          .setName('furmanism')
-	          .setDescription('truly, it never ends.'),
+	          .setDescription('truly, it never ends.')
+		  .addStringOption(option =>
+			option
+				.setName('index')
+				.setDescription(`[optional] pick an index between 0 and ${cmdlen}`)),
 	      async execute(interaction) {
-	      	      await interaction.reply(randomChoice(furmanisms))
+		      const index = interaction.options.getString('index') ?? '-1';
+		      if (index == '-1') {
+	      	      	await interaction.reply(randomChoice(furmanisms))
+		      }
+		      const numIndex = index*1
+		      if(isNumeric(numIndex) && numIndex >= 0 && numIndex < cmdlen) {
+			      await interaction.reply(furmanisms[numIndex])
+		      } else {
+			      await interaction.reply(randomChoice(furmanisms))
+		      }
 	      },
 };
